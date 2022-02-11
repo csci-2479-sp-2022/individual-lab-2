931550d8e4ba57a4a830cca4da77afd526f687dc
Mihaela Valac <mvalac@student.cscc.edu>
2022-02-11 15:27:05 -0500

    Created Pet model, view, route, and controller.

---
+++ app/Http/Controllers/PetController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pet;
+use Illuminate\Http\Request;
+
+class PetController extends Controller  {
+    public function show(){
+        return view (
+            'pets',
+            [
+                'pets' => self::getPet(),
+            ]);
+            return null;
+    }
+
+    public static function getPet() {
+        return [
+            Pet::make(['name' => 'Salem', 'age' => 8, 'type' => 'dog']),
+            Pet::make(['name' => 'Magic', 'age' => 11, 'type' => 'dog']),
+            Pet::make(['name' => 'Garfield', 'age' => 40, 'type' => 'cat'])
+        ];
+    }
+
+}
