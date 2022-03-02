735cf638bedbe032af9345c21aea92e24a9a41aa
Tadios Desta <tdesta@student.cscc.edu>
2022-03-02 09:52:02 -0500

    Create a model, controller, route, and view using of Acceptance Criteria Lab 2

--- app/Models/Pet.php
+++ app/Models/Pet.php
@@ -1,5 +1,27 @@
-protected $fillable = [
-    'name',
-    'age',
-    'type'
-];
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pet extends Model{
+
+    use HasFactory;
+
+    //define model attributes (more on this when we talk about Eloquent):
+
+    protected $fillable = [
+        'name',
+        'age',
+        'type'
+    ];
+    //Add a public toString() function that outputs the fillable properties into a descriptive string about the pet.
+
+    public function toString(){
+
+        return "$this->name . 'is a ' . $this->age . ' years old, and is a ' .  $this->type .";
+
+    }
+}
